2aa1b8b99623b6ba4c954e6d6b705c50a96dfa18
Sophie Tai <tai_sophie@hotmail.com>
2022-04-04 11:48:14 +0800

    [IT WORKED. THANK YOU JESUS]

--- app/src/main/java/com/example/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.kt
@@ -4,14 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.temperatureconverter.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class ActivityMain : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.convertButton.setOnClickListener { fahrenheitCelcius() }
     }
 
+    private fun fahrenheitCelcius() {
+        val stringInTextField = binding.temperatureInput.text.toString()
+        val temperature = stringInTextField.toDouble()
+        if(temperature == null){
+            binding.resultText.text = ""
+            return
+        }
+        val celciusTemp = temperature + 32/1.8
+        val fahrTemp = temperature -32 / 1.8
+        val kelvTemp = temperature + 273
+
+        when (binding.temperatureOptions.checkedRadioButtonId) {
+            R.id.option_degree_celcius -> {
+                binding.resultText.text = celciusTemp.toString()
+            }
+            R.id.option_degree_fahrenheit -> {
+                 binding.resultText.text = fahrTemp.toString()
+            }
+            else -> {
+                binding.resultText.text = kelvTemp.toString()
+            }
+        }
+    }
 }
